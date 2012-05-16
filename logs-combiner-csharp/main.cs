05f4344c5cb74cbd5d5f230f73817be2d2e0b801
Jose A Maestre Celdran <jomaceld@gmail.com>
2012-05-17 00:03:03 +0100

    * Added support for multiple variables per timestamp
    Signed-off-by: Jose A Maestre Celdran <jomaceld@gmail.com>

--- logs-combiner-csharp/main.cs
+++ logs-combiner-csharp/main.cs
@@ -8,6 +8,7 @@ namespace ConsoleApplication1
     class main
     {
         public static bool verbose = true;
+        public static bool debug = false;
         public static SortedDictionary<string, List<Data>> dataDictionary;
 
         static void Main(string[] args)
@@ -32,13 +33,18 @@ namespace ConsoleApplication1
         /// <summary>
         /// Read the file specified by the parameter fileName.
         /// </summary>
-        /// <param name="fileName"></param>
+        /// <param name="fileName">Path to the file.</param>
         /// <param name="format">
+        /// Format used to extract the data from each line of the file.
+        /// ----------------------------
+        /// Sintax (each type of value must be separated by ';'):
+        /// 
         /// %ut --> Linux timeStamp
         /// %i --> ID
         /// %v --> Value
         /// %vn -- Value Name
-        /// e.g. "%t;%i;%v;%v" --> Time,ID,Value1,Value2
+        /// 
+        /// e.g. "%t;%i;%v;%v;%vn;%vn" --> Time,ID,Value1,Value2,ValueName1,ValueName2
         /// </param>
         public static void readFile(string fileName,string format)
         {
@@ -98,6 +104,23 @@ namespace ConsoleApplication1
                             }
                             Console.WriteLine("\n-------------------------");
                         }
+
+                        // Create Data Object
+                        Data d = new Data(id, values, valueNames);
+                        //Store Data object in to the dictionary
+                        if (dataDictionary.Keys.Contains(timestamp))
+                        {
+                            if (debug) {
+                                Console.WriteLine("Timestamp already found in the dictionary");
+                            }
+                            dataDictionary[timestamp].Add(d);
+
+                        } else {
+                            
+                            List<Data> auxList = new List<Data>();
+                            auxList.Add(d);
+                            dataDictionary.Add(timestamp, auxList);
+                        }
                     }
                     counter++;
                 }
@@ -109,6 +132,21 @@ namespace ConsoleApplication1
     class Data
     {
         string id;
-        string value;
+        List<string> values = new List<string>();
+        List<string> valueNames = new List<string>();
+
+        /// <summary>
+        /// Data Constructor
+        /// </summary>
+        /// <param name="i"> ID of the data </param>
+        /// <param name="valuesList"> list of values </param>
+        /// <param name="valuesNamesList"> list of names of the values </param>
+        public Data(string i, List<string> valuesList, List<string> valuesNamesList)
+        {
+            this.id = i;
+            this.values = valuesList;
+            this.valueNames = valuesNamesList;
+        
+        }
     }
 }
